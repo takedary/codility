a3a7d291062dee41e40bb587a584477480295efd
takedary <sb.rtakeda@gmail.com>
2021-01-05 11:30:18 +0900

    add 4-2

---
+++ l4_2_MaxCounters.py
@@ -0,0 +1,41 @@
+from collections import defaultdict
+
+
+def solution(N, A):
+  max_count = 0
+  sector_counter = defaultdict(int)
+  sector_max = 0
+  for a in A:
+    if a == N + 1 and sector_counter:
+      max_count += sector_max
+      sector_counter.clear()
+      sector_max = 0
+    else:
+      sector_counter[a] += 1
+      sector_max = max(sector_max, sector_counter[a])
+
+  return [max_count + sector_counter[k] for k in range(1, N+1)]
+
+
+def slow_solution(N, A):
+  counter = {i: 0 for i in range(1, N+1)}
+  for a in A:
+    if a == N + 1:
+      max_count = max(counter.values())
+      for k in counter.keys():
+        counter[k] = max_count
+    else:
+      counter[a] += 1
+  return list(counter.values())
+
+
+if __name__ == '__main__':
+  cases = [
+      (5, [3, 4, 4, 6, 1, 4, 4]),
+      (5, [3, 4, 4, 6, 1, 1, 1, 6, 3]),
+      (5, [6, 6]),
+  ]
+  for c in cases:
+    print(f'\n{c = }')
+    print(f'{solution(*c) = }')
+    print(f'{slow_solution(*c) = }')
