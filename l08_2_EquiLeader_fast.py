6f2d7bc3a38df3afcbeb83ac41da719edadb9565
takedary <sb.rtakeda@gmail.com>
2021-01-26 17:25:22 +0900

    refactor

--- l08_2_EquiLeader_fast.py
+++ l08_2_EquiLeader_fast.py
@@ -1,5 +1,11 @@
 def bm_leader(A):
-  """Boyer–Moore majority vote algorithm."""
+  """Boyer–Moore majority vote algorithm.
+
+  ValueError is raised if no leader exists.
+  """
+  if not A:
+    raise ValueError
+
   candidate = A[0]
   count = 1
   for a in A[1:]:
@@ -25,12 +31,12 @@ def solution(A):
   except ValueError:
     return 0
 
-  n_els = 0
+  n_els = 0  # num of equi leaders
   count_left = 0
-  for i, a in enumerate(A, start=1):
+  for i, a in enumerate(A, 1):
     if a == leader:
       count_left += 1
-    if 2 * count_left > i and 2 * (count-count_left) > N - i:
+    if count_left > i // 2 and count - count_left > (N-i) // 2:
       n_els += 1
   return n_els
 
