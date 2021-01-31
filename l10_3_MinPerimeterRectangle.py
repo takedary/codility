90c59086ca866ad4fb11ae486a5cf697ec242ccd
takedary <sb.rtakeda@gmail.com>
2021-02-01 01:02:14 +0900

    refactor

--- l10_3_MinPerimeterRectangle.py
+++ l10_3_MinPerimeterRectangle.py
@@ -1,8 +1,15 @@
-import math
+from math import sqrt
 
 
 def solution(N):
-  a = int(math.sqrt(N))
-  for i in range(a, 0, -1):
-    if N % i == 0:
-      return 2 * (i + int(N/i))
+  s = int(sqrt(N))
+  for a in range(s, 0, -1):
+    if N % a == 0:
+      return 2 * (a + N//a)
+
+
+if __name__ == '__main__':
+  cases = [(1,), (2,), (3,), (4,), (5,), (6,), (7,), (8,), (9,), (10,), (30,)]
+  for c in cases:
+    print(f'\n{c = }')
+    print(f'{solution(*c) = }')
