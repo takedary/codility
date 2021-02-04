dde4fa2f32055e0216470823a76af24ae301c62e
takedary <sb.rtakeda@gmail.com>
2021-02-05 01:18:09 +0900

    clean up

--- l10_2_Flags.py
+++ l10_2_Flags.py
@@ -1,89 +1,7 @@
-from itertools import accumulate
 from math import sqrt
 
 
-def check(peaks, n_flags_taken):
-  n_flags_set = 1
-  start = peaks[0]
-  flags = [start]
-  for p in peaks[1:]:
-    if p - start >= n_flags_taken:
-      n_flags_set += 1
-      flags.append(p)
-      start = p
-      if n_flags_set == n_flags_taken:
-        break
-  print('n_flags_taken = {}, flags = {}, n_flags_set = {}'.format(
-        n_flags_taken, flags, n_flags_set))
-  return n_flags_set
-
-def solution(A):
-  N = len(A)
-  if N < 3:
-    return 0
-
-  # find peaks
-  peaks = [i for i in range(1, N-1) if A[i-1] < A[i] and A[i] > A[i+1]]
-  print('peaks:', peaks)
-  n_peaks = len(peaks)
-  if n_peaks <= 2:
-    return n_peaks
-
-  # find maximum number of flags to check
-  max_peak_distance = peaks[-1] - peaks[0]
-  max_flags = n_peaks
-  print('max_flags:', max_flags)
-  while max_flags * (max_flags-1) > max_peak_distance:
-    max_flags -= 1
-  print('max_flags:', max_flags)
-  if max_flags == 2:
-    return 2
-
-  # check
-  for n_flags_taken in range(max_flags, 2, -1):
-    n_flags_set = check(peaks, n_flags_taken)
-    if n_flags_set == n_flags_taken:
-      return n_flags_set
-  return 2
-
-
-'''
-def f(peaks):
-  nflags = 3
-  max_nflags_set = 2
-  while True:
-    for left in range(len(peaks)):
-      count = 1
-      start = peaks[left]
-      flags = [start]
-      for p in peaks[left+1:]:
-        if p - start >= nflags:
-          count += 1
-          flags.append(p)
-          start = p
-          if count == nflags:
-            break
-      print('nflags = {}, left = {}, flags = {}, count = {}'.format(
-            nflags, left, flags, count))
-      if count > max_nflags_set:
-        max_nflags_set = count
-    if max_nflags_set < nflags:
-      break
-    nflags += 1
-  return max_nflags_set
-
-def slow_solution(A):
-  # find peaks
-  peaks = [i for i in range(1, len(A) - 1) if A[i-1] < A[i] and A[i] > A[i+1]]
-  print('peaks:', peaks)
-  if len(peaks) <= 2:
-    return len(peaks)
-
-  return f(peaks)
-'''
-
-
-def solution1(A):
+def smarter_solution(A):
   N = len(A)
   if N <= 2:
     return 0
@@ -125,7 +43,7 @@ def solution1(A):
   return 2
 
 
-def solution2(A):
+def faster_solution(A):
   N = len(A)
   if N <= 2:
     return 0
@@ -177,8 +95,8 @@ if __name__ == '__main__':
 
   for c in cases:
     print(f'\n{c = }')
-    print(f'{solution1(*c) = }')
-    print(f'{solution2(*c) = }')
+    print(f'{smarter_solution(*c) = }')
+    print(f'{faster_solution(*c) = }')
 
 
   # speed test
@@ -208,11 +126,11 @@ if __name__ == '__main__':
 
   def test_golden(cases):
     for c in cases:
-      solution1(*c)
+      smarter_solution(*c)
 
   def test_mine(cases):
     for c in cases:
-      solution2(*c)
+      faster_solution(*c)
 
   def run_test(name, test_str):
     print(name)
