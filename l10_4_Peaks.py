98f7937699609cdd871316c4e1b1cc3b8bf116c1
takedary <sb.rtakeda@gmail.com>
2021-02-07 00:27:28 +0900

    add simpler and faster solution

--- l10_4_Peaks.py
+++ l10_4_Peaks.py
@@ -1,3 +1,46 @@
+from itertools import accumulate
+from math import sqrt
+
+
+def second_max_divisor(N):
+  for i in range(2, int(sqrt(N)) + 1):
+    if N % i == 0:
+      return N // i
+  return 1
+
+def solution1(A):
+  N = len(A)
+  if N <= 2:
+    return 0
+
+  is_peak = [int(A[i-1] < A[i] and A[i] > A[i+1]) for i in range(1, N-1)]
+  is_peak = [0] + is_peak + [0]
+  acc_peaks = list(accumulate(is_peak, initial=0))
+  n_peaks = acc_peaks[-1]
+  if n_peaks <= 1:
+    return n_peaks
+
+  max_n_blocks = min(n_peaks, second_max_divisor(N))
+  if max_n_blocks == 1:
+    return 1
+
+  def has_no_peak(block_idx):
+    return acc_peaks[(block_idx - 1) * bsize] == acc_peaks[block_idx * bsize]
+
+  for n_blocks in range(max_n_blocks, 1, -1):
+    if N % n_blocks != 0:
+      continue
+
+    bsize = N // n_blocks
+    if any(has_no_peak(i) for i in range(1, n_blocks + 1)):
+      continue
+    return n_blocks
+
+  return 1
+
+
+#### slower solution
+
 def find_divisors(N):
   divisors0, divisors1 = [], []
   d = 2
@@ -31,7 +74,7 @@ def find_peaks(A, N):
 
 def check_block_size(A, cum_ps, N, block_size):
   cp = [cum_ps[i * block_size - 1] for i in range(1, N // block_size + 1)]
-  print('block_size = {}, cp = {}'.format(block_size, cp))
+  #print('block_size = {}, cp = {}'.format(block_size, cp))
   return cp[0] > 0 and all(c0 < c1 for c0, c1 in zip(cp[:-1], cp[1:]))
 
 def search(A, cum_ps, N, block_sizes):
@@ -40,24 +83,24 @@ def search(A, cum_ps, N, block_sizes):
       return N // block_size
   return 1
 
-def solution(A):
+def solution2(A):
   N = len(A)
-  print('N = {}'.format(N))
+  #print('N = {}'.format(N))
   if N <= 2:
     return 0
 
   divisors = find_divisors(N)
-  print('divisors = {}'.format(divisors))
+  #print('divisors = {}'.format(divisors))
   
   cum_ps, min_block_size = find_peaks(A, N)
-  print(cum_ps, min_block_size)
+  #print(cum_ps, min_block_size)
   if cum_ps[-1] <= 1:
     return cum_ps[-1]
   elif not divisors:
     return 1
 
   block_sizes = [d for d in divisors if d >= min_block_size]
-  print('block_sizes = {}'.format(block_sizes))
+  #print('block_sizes = {}'.format(block_sizes))
   return search(A, cum_ps, N, block_sizes)
 
 
@@ -74,6 +117,7 @@ if __name__ == '__main__':
       ([1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 2, 2],),
       ([1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],),
       ([0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0],),
+      ([0, 1, 0, 0, 1, 0, 0, 1, 0],),
   ]
   cases.extend([
       (random.choices([0, 1], k=random.randrange(6, 400)),)
@@ -81,4 +125,5 @@ if __name__ == '__main__':
   ])
   for c in cases:
     print(f'\n{c = }')
-    print(f'{solution(*c) = }')
+    print(f'{solution1(*c) = }')
+    print(f'{solution2(*c) = }')
