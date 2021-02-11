926d7964d1093d5e60398f2d91b0de80bc16e1f0
takedary <sb.rtakeda@gmail.com>
2021-02-12 01:57:34 +0900

    add tests

--- l11_2_CountSemiprimes.py
+++ l11_2_CountSemiprimes.py
@@ -24,11 +24,11 @@ def solution(N, P, Q):
 
   # O(NloglogN)
   f = arrayf(maxq)
-  print_table(f)
+  #print_table(f)
 
   # O(N)
   pref = list(accumulate(f[i] > 0 and f[i//f[i]] == 0 for i in range(maxq+1)))
-  print_table(pref)
+  #print_table(pref)
 
   # O(M)
   return [pref[q] - pref[p-1] for p, q in zip(P, Q)]
@@ -47,7 +47,7 @@ def slow_solution(N, P, Q):
 
   # make set of primes: O(N log(logN))
   s = primes(maxq // 2)
-  print(s)
+  #print(s)
 
   # list semiprimes: O((N/logN)^2) -- 素数定理
   semiprimes = [0] * (maxq + 1)
@@ -57,15 +57,52 @@ def slow_solution(N, P, Q):
         semiprimes[a * b] = 1
       except IndexError:
         pass
-  print_table(semiprimes)
+  #print_table(semiprimes)
 
   # prefix sum: O(N)
   pref = list(accumulate(semiprimes))
-  print_table(pref)
+  #print_table(pref)
 
   return [pref[q] - pref[p-1] for p, q in zip(P, Q)]
 
 
 if __name__ == '__main__':
-  print(slow_solution(None, [1, 4, 16, 30], [26, 10, 20, 42]))
-  print(solution(None, [1, 4, 16, 30], [26, 10, 20, 42]))
+  from random import randrange
+  import timeit
+
+  cases = [
+      (None, [1, 4, 16, 30], [26, 10, 20, 42]),
+  ]
+  for c in cases:
+    print(f'\n{c = }')
+    print(f'{solution(*c) = }')
+
+
+  print('\nspeed test')
+
+  def setup(N):
+    cases = []
+    for _ in range(10):
+      M = 100
+      P = [randrange(1, N+1) for _ in range(M)] + [N]
+      Q = [randrange(p, N+1) for p in P]
+      cases.append((None, P, Q))
+    return cases
+
+  def test_solution(solution, cases):
+    for c in cases:
+      solution(*c)
+
+  def run_test(test_str, start, stop):
+    print(test_str)
+    globals_ = globals()
+    for N in [10**i for i in range(start, stop)]:
+      globals_['N'] = N
+      times = timeit.repeat(
+          test_str, 'cases = setup(N)', globals=globals_,
+          repeat=5, number=1
+      )
+      print(f'{N = }', '{:.6f}'.format(sum(times)/50), sep='\t')
+
+  run_test('test_solution(solution, cases)', 2, 6)
+  run_test('test_solution(slow_solution, cases)', 2, 5)
