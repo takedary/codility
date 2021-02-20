33354e6f33ebca3095d0294c2d2c89f441f9d70e
takedary <sb.rtakeda@gmail.com>
2021-02-20 21:22:25 +0900

    refactor

--- l13_2_Ladder.py
+++ l13_2_Ladder.py
@@ -1,14 +1,17 @@
 def solution(A, B):
-  mask = 2 ** max(B) - 1
-  ways = [1, 1]
+  mask = (1 << max(B)) - 1
+
+  nways = [1, 1]
   for _ in range(2, max(A) + 1):
-    ways.append((ways[-2] + ways[-1]) & mask)
-  return [ways[a] % 2**b for a, b in zip(A, B)]
+    nways.append((nways[-2] + nways[-1]) & mask)
+
+  return [nways[a] & ((1 << b) - 1) for a, b in zip(A, B)]
 
 
 if __name__ == '__main__':
   cases = [
       ([1, 2, 3, 4, 5, 6, 15], [3, 2, 1, 3, 2, 1, 20]),
+      ([4, 4, 5, 5, 1], [3, 2, 4, 3, 1]),
   ]
   for c in cases:
     print(f'\n{c = }')
