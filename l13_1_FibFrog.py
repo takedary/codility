04e5513a3339f4e208e258445bfd952847866260
takedary <sb.rtakeda@gmail.com>
2021-02-23 00:18:14 +0900

    add a better implementation

--- l13_1_FibFrog.py
+++ l13_1_FibFrog.py
@@ -1,3 +1,9 @@
+from functools import lru_cache
+import sys
+
+sys.setrecursionlimit(10000)
+
+
 def gen_fibo(m):
   """
   return fibonacci seq while <= m, ommiting first two numbers
@@ -8,6 +14,41 @@ def gen_fibo(m):
     a, b = b, a + b
 
 def solution(A):
+  if not A:
+    return 1
+
+  N = len(A)
+
+  fibos = list(gen_fibo(N + 1))
+  fibos_set = set(fibos)
+  if N + 1 in fibos_set:
+    return 1
+
+  @lru_cache(maxsize=N+1)
+  def min_jumps_from(start):
+    """
+    Return min num of jumps to reach from position `start` to the other side
+    of the river (position N), or return -1 if cannot reach.
+    """
+    if start == N:
+      return 0
+    if N - start in fibos_set:
+      return 1
+
+    min_min_jumps = N + 2
+    for f in reversed(fibos): ## reversed is faster??
+      if (idx := start + f) < N and A[idx]:
+        if (min_jumps := min_jumps_from(idx)) == 1:
+          return 2
+        if min_jumps > 1:
+          min_min_jumps = min(min_min_jumps, min_jumps)
+
+    return min_min_jumps + 1 if min_min_jumps < N + 2 else -1
+
+  return min_jumps_from(-1)
+
+
+def solution1(A):
   if not A:
     return 1
 
@@ -47,15 +88,36 @@ def solution(A):
 
   return min_jumps(N)
 
+
 if __name__ == '__main__':
+  from random import randrange, sample
+
   cases = [
+      # (leaves, N)
+      ([], 0),
+      ([], 1),
+      ([], 4),
+      ([], 5),
       ([4, 7, 8, 13], 15),
       ([3, 4, 6], 11),
   ]
+  for _ in range(6000):
+    N = randrange(64)
+    A = sample(range(N), randrange(N + 1))
+    cases.append((A, N))
+  for _ in range(60):
+    N = randrange(3)
+    A = sample(range(N), randrange(N + 1))
+    cases.append((A, N))
+
   for c in cases:
-    cc = [0] * c[1]
+    A = [0] * c[1]
     for i in c[0]:
-      cc[i] = 1
-    c = (cc,)
+      A[i] = 1
+    c = (A,)
+    if solution(*c) == solution1(*c):
+      continue
     print(f'\n{c = }')
     print(f'{solution(*c) = }')
+    print(f'{solution1(*c) = }')
+    #print(f'{solution2(*c) = }')
